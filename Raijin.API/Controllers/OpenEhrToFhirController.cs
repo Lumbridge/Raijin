5824caf02678f03ce99db3a8145830c72d4ee900
Lumbridge <ryansainty555@hotmail.com>
2021-04-06 23:00:35 +0100

    Added API endpoint for converting an openEHR message into a FHIR bundle.

---
+++ Raijin.API/Controllers/OpenEhrToFhirController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+using Microsoft.Extensions.Logging;
+using Raijin.Core.Classes;
+using Raijin.Core.Helpers;
+
+namespace Raijin.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OpenEhrToFhirController : ControllerBase
+    {
+        private readonly ILogger<OpenEhrToFhirController> _logger;
+
+        public OpenEhrToFhirController(ILogger<OpenEhrToFhirController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]string openEhrMessage)
+        {
+            if (string.IsNullOrEmpty(openEhrMessage))
+                return BadRequest();
+
+            var acceptHeader = Request.Headers["Accept"].FirstOrDefault();
+            
+            if (string.IsNullOrEmpty(acceptHeader))
+                acceptHeader = "application/json";
+
+            var messageHandler = new MessageHandler();
+            
+            var processedMessage = messageHandler.ProcessMessage(openEhrMessage);
+
+            var result = acceptHeader.Contains("xml")
+                ? processedMessage.SerializedResourceBundleXml
+                : processedMessage.SerializedResourceBundleJson;
+
+            return Content(result); 
+        }
+    }
+}
